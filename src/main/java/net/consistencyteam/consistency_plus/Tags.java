f836848467b482cef6f8b2a9d42086a7f3324d7f
Siuol <Lmeitrodt@gmail.com>
2021-04-04 19:32:22 -0500

    And when this Red runs Black...
    In this commit I did the following:
    
    Completed the remaining blocks needed for 0.3. Theres like a 60% chance Ill add some additional gates and an 100% chance for a last minute block addition.
    Added Soul Fire/Soul Speed support for the Soul Sandstone blocks.
    Added a file for registering certain tags, it isnt initalized so its fine.
    Cleaned up some jsons
    Fixed some models
    Bump to 0.3.0-Beta
    
    I'll ask you if Im everything you imagined.

---
+++ src/main/java/net/consistencyteam/consistency_plus/Tags.java
@@ -0,0 +1,23 @@
+package net.consistencyteam.consistency_plus;
+
+import net.fabricmc.fabric.api.tag.TagRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.tag.Tag;
+import net.minecraft.util.Identifier;
+
+// This code is based on code from Draylar's "Tiered" mod, found here: https://github.com/Draylar/tiered/blob/1.16/src/main/java/draylar/tiered/api/TieredItemTags.java
+// That code is licensed under MIT.
+// Thanks for your work, Draylar!
+
+// Also, code is not being registered/initalized due to the fact that it does not have its usage JUST yet.
+
+public class Tags {
+    public static final Tag<Block> GATES = register("gates");
+
+    private static Tag<Block> register(String id) {
+        return TagRegistry.block(new Identifier("consistency_plus", id));
+    }
+
+    public static void init() {
+    }
+}

8bff24acb96371450a1b1da70c47a653d8ce9eda
Tropheus Jay <jverrellijr@icloud.com>
2021-10-31 22:23:09 -0400

    fix the server WTHIT plugin error

--- src/main/java/io/github/consistencyplus/consistency_plus/blocks/nubert/NubertOverride.java
+++ src/main/java/io/github/consistencyplus/consistency_plus/blocks/nubert/NubertOverride.java
@@ -4,13 +4,11 @@ import mcp.mobius.waila.api.IBlockAccessor;
 import mcp.mobius.waila.api.IBlockComponentProvider;
 import mcp.mobius.waila.api.IPluginConfig;
 import mcp.mobius.waila.api.ITooltip;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.minecraft.util.math.BlockPos;
 
-@Environment(EnvType.CLIENT)
 public class NubertOverride implements IBlockComponentProvider {
 	private BlockPos lastPos;
+	
 	@Override
 	public void appendBody(ITooltip tooltip, IBlockAccessor accessor, IPluginConfig config) {
 		BlockPos pos = accessor.getPosition();

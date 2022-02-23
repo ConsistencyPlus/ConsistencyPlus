e9a0e307ed4a5ecee9d2f4593fbbe1db7c748d54
Siuol <43890828+Siuolthepic@users.noreply.github.com>
2022-02-22 23:10:26 -0600

    Motherboard had to go in to surgery during the time between commits.
    Updated textures, added 2 translations from Dodomos
    
    You know the concern emoji, well post it here.
    
    Signed-off-by: Siuol <43890828+Siuolthepic@users.noreply.github.com>

--- fabric/src/main/java/io/github/consistencyplus/consistency_plus/ConsistencyPlus.java
+++ fabric/src/main/java/io/github/consistencyplus/consistency_plus/ConsistencyPlus.java
@@ -2,6 +2,12 @@ package io.github.consistencyplus.consistency_plus;
 
 import io.github.consistencyplus.consistency_plus.base.ConsistencyPlusMain;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.tutorial.FindTreeTutorialStepHandler;
+import net.minecraft.client.tutorial.TutorialManager;
+import net.minecraft.entity.ai.brain.sensor.PiglinSpecificSensor;
+import net.minecraft.entity.mob.PiglinBrain;
+import net.minecraft.entity.mob.PiglinEntity;
+import net.minecraft.screen.StonecutterScreenHandler;
 
 public class ConsistencyPlus implements ModInitializer {
 	@Override

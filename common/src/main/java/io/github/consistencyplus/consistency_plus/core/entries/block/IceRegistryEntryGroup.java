6d50aca14cf3a40cd4cfb33c29ec9feabe470970
Siuol <43890828+Siuolthepic@users.noreply.github.com>
2022-07-04 20:10:29 -0500

    ACLs are surprisingly easy to tear
    Anyways I added ice translucency using a bandaid. Also Forge doesnt work and IDK why
    
    Signed-off-by: Siuol <43890828+Siuolthepic@users.noreply.github.com>

--- common/src/main/java/io/github/consistencyplus/consistency_plus/core/entries/block/IceRegistryEntryGroup.java
+++ common/src/main/java/io/github/consistencyplus/consistency_plus/core/entries/block/IceRegistryEntryGroup.java
@@ -8,13 +8,19 @@ import io.github.consistencyplus.consistency_plus.core.extensions.CPlusFenceGate
 import io.github.consistencyplus.consistency_plus.core.extensions.CPlusStairBlock;
 import net.minecraft.block.*;
 
+import java.util.ArrayList;
+
 public class IceRegistryEntryGroup extends MiscRegistryEntryGroup {
+    // This is static because Im lazy
+    public static ArrayList<RegistrySupplier<Block>> iceBlocks = new ArrayList<>();
+
     public IceRegistryEntryGroup(String name, AbstractBlock.Settings blockSettings) {
         super(name, blockSettings);
     }
 
+    // Todo: Yeah this is why I need to redo this
     public RegistrySupplier<Block> blockRegistration(String name, BlockShapes blockShapes, AbstractBlock.Settings blockSettings) {
-        return switch (blockShapes) {
+        RegistrySupplier<Block> a = switch (blockShapes) {
             default -> ConsistencyPlusMain.BLOCKS.register(name, () ->  new IceBlock(blockSettings));
             case SLAB -> ConsistencyPlusMain.BLOCKS.register(name, () -> new IceSlabBlock(blockSettings));
             case STAIRS -> ConsistencyPlusMain.BLOCKS.register(name, () ->  new IceStairsBlock(Blocks.ICE.getDefaultState(), blockSettings));
@@ -23,5 +29,7 @@ public class IceRegistryEntryGroup extends MiscRegistryEntryGroup {
             case GATE -> ConsistencyPlusMain.BLOCKS.register(name, () ->  new IceGateBlock(blockSettings));
             case PILLAR -> ConsistencyPlusMain.BLOCKS.register(name, () ->  new IcePillarBlock(blockSettings));
         };
+        iceBlocks.add(a);
+        return a;
     }
 }

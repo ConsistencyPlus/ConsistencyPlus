52dc43d0f78dd12bdf9cf417a455bc700b66beff
TropheusJ <jverrellijr@icloud.com>
2023-01-16 06:17:55 -0500

    how

--- fabric/src/main/java/io/github/consistencyplus/consistency_plus/data/providers/ConsistencyPlusTagProviderFabric.java
+++ fabric/src/main/java/io/github/consistencyplus/consistency_plus/data/providers/ConsistencyPlusTagProviderFabric.java
@@ -3,8 +3,7 @@ package io.github.consistencyplus.consistency_plus.data.providers;
 import io.github.consistencyplus.consistency_plus.data.ConsistencyPlusTagProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
-
-import javax.annotation.Nullable;
+import org.jetbrains.annotations.Nullable;
 
 public class ConsistencyPlusTagProviderFabric {
 

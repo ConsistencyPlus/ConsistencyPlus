5c13c882db29aa5dfce500a602e5531aec3e371f
TropheusJ <jverrellijr@icloud.com>
2023-12-29 18:50:40 -0500

    bump forge (fix #184), fix #186 also needed to glue SCRT back together also also bumped to RC 5

--- forge/src/main/java/io/github/consistencyplus/consistency_plus/forge/WitheredBonesModifier.java
+++ forge/src/main/java/io/github/consistencyplus/consistency_plus/forge/WitheredBonesModifier.java
@@ -4,20 +4,24 @@ import com.google.common.base.Suppliers;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.mob.WitherSkeletonEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.loot.condition.LootCondition;
 import net.minecraft.loot.context.LootContext;
-import net.minecraft.registry.Registries;
+import net.minecraft.loot.context.LootContextParameters;
 import net.minecraft.util.Identifier;
-import net.minecraft.registry.Registry;
 import net.minecraftforge.common.loot.IGlobalLootModifier;
 import net.minecraftforge.common.loot.LootModifier;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Supplier;
 
 public class WitheredBonesModifier extends LootModifier {
+    public static final Identifier WITHERED_BONE_ID = new Identifier("consistency_plus", "withered_bone");
+
     public static final Supplier<Codec<WitheredBonesModifier>> CODEC = Suppliers.memoize(() -> RecordCodecBuilder.create(inst -> codecStart(inst)
             .apply(inst, WitheredBonesModifier::new)
     ));
@@ -27,8 +31,17 @@ public class WitheredBonesModifier extends LootModifier {
     }
 
     @Override
+    @NotNull
     protected ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
-        generatedLoot.add(Registries.ITEM.get(new Identifier("consistency_plus", "withered_bone")).getDefaultStack());
+        Entity entity = context.get(LootContextParameters.THIS_ENTITY);
+        if (entity instanceof WitherSkeletonEntity) {
+            Item bone = ForgeRegistries.ITEMS.getValue(WITHERED_BONE_ID);
+            if (bone == null) {
+                throw new IllegalStateException("Withered Bone does not exist?");
+            }
+            generatedLoot.add(new ItemStack(bone));
+        }
+
         return generatedLoot;
     }
 
